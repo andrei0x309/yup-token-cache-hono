555e26b44cd8d4c06981d2e9e3fae92489694749
Andrei O <andrei@flashsoft.eu>
2024-01-01 17:50:14 +0200

    chore: update endpoint

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# Hono Deno REST API Template
+# Hono Deno KV Cache layer
 
-A template of REST API app using [Hono](https://hono.dev/) framework
-
-```
-deno run --allow-net main.ts
-```
-
-This starts the server at http://localhost:8000/
-
-Try go to http://localhost:8000/api/Brachiosaurus or http://localhost:8000/api/
+This is a simple key-value cache layer using KV, Deno and Hono.
--- main.ts
+++ main.ts
@@ -1,20 +1,62 @@
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import data from "./data.json" assert { type: "json" };
 
 const app = new Hono();
+const kv = await Deno.openKv();
 
-app.get("/", (c) => c.text("Welcome to dinosaur API!"));
+const getData = async (token: string) => {
+  try {
+  const API_URL = Deno.env.get("API_URL");
+ 
+  const res = await fetch(`${API_URL}`, {
+    headers: {
+      authorization: token,
+    },
+  });
+  const data = await res.json();
+  return data;
+} catch (e) {
+  return null;
+}
+}
 
-app.get("/api/", (c) => c.json(data));
-
-app.get("/api/:dinosaur", (c) => {
-  const dinosaur = c.req.param("dinosaur").toLowerCase();
-  const found = data.find((item) => item.name.toLowerCase() === dinosaur);
-  if (found) {
-    return c.json(found);
-  } else {
-    return c.text("No dinosaurs found.");
+app.get("/token/yup", async (c) => {
+  const API_URL = Deno.env.get("API_URL");
+  if (!API_URL) {
+    return c.json({ error: "API_URL not found" });
   }
-});
+  const token = c.req.headers.get("authorization");
+  if (!token) {
+    return c.json({ error: "token not found" });
+  }
 
-Deno.serve(app.fetch);
+  const reqData = await kv.get(["preferences", "data"]);
+
+  if (!reqData) {
+    const data = await getData(token);
+    if (!data) {
+      return c.json({ error: "data not found" });
+    }
+    const storeData = {} as Record<string, any>;
+    storeData.data = data;
+    storeData.timestamp = Date.now();
+    await kv.set(["preferences", "data"], storeData);
+    return c.json(data);
+  }
+  
+  const { timestamp } = reqData;
+  const now = Date.now();
+  const diff = now - timestamp;
+  const minutes = Math.floor(diff / 1000 / 60);
+  if (minutes > 110) {
+    const data = await getData(token);
+    if (!data) {
+      return c.json(reqData.data);
+    }
+    const storeData = {} as Record<string, any>;
+    storeData.data = data;
+    storeData.timestamp = Date.now();
+    await kv.set(["preferences", "data"], storeData);
+    return c.json(data);
+  }
+  return c.json(reqData.data);
+});
